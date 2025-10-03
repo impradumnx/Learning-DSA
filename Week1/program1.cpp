2fe3388069ad2f5278691d66def1101ebff2e43e
Pradum <pradumrathour944@gmail.com>
2025-10-03 16:19:12 +0530

    program 1

---
+++ Week1/program1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    int sum = 0;
+    cout << "Enter a number : ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    cout << "The sum of numbers : " << sum;
+    return 0;
+}
