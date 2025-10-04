7ef919a093c11a6043e2d0a5dd5ba62c3a22cbfc
Pradum <pradumrathour944@gmail.com>
2025-10-04 21:05:52 +0530

    type casting

--- Week1/program3.cpp
+++ Week1/program3.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main (){
     float a = 20.18f;
     int b = (int)a;
-    cout << b << endl << sizeof(b); 
+    cout << b << endl << sizeof(b) << endl; 
+    double c = (double)b;
+    cout << c;
     return 0;
 }
+
