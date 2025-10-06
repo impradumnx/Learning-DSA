f155dd8ea2ef8401707e9a456158ea991f6dab64
Pradum <pradumrathour944@gmail.com>
2025-10-06 22:35:58 +0530

    patern of alphabets

---
+++ Week2/program2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        char ch = 'A';
+        for(int j=1; j<=n; j++){
+            cout << ch << "  ";
+            ch += 1;
+        } cout << endl;
+    }
+    return 0;
+}
