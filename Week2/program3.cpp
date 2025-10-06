a16604e4c0c29dc034a46f60295e1e15bda93d09
Pradum <pradumrathour944@gmail.com>
2025-10-06 22:54:10 +0530

    another numeric pattern

---
+++ Week2/program3.cpp
@@ -0,0 +1,17 @@
+//integer pattern 2
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int num = 1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout << num << "  ";
+            num += 1;
+        } cout << endl;
+    }
+    return 0;
+}
