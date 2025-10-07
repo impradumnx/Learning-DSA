2535ff01d16226c35c207214c001a3bc8d5e87ed
Pradum <pradumrathour944@gmail.com>
2025-10-07 08:18:34 +0530

    modified

--- Week2/program6.cpp
+++ Week2/program6.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char ch ='A';
     int n;
     cout << "Enter n : ";
     cin >> n;
     for(int i=0; i<n; i++){
         for(int j=0; j<i+1; j++){
-            cout << ch <<" ";
-        }
-        cout << "\n";
-        ch += 1;
+            cout << i+1 <<" ";
+        } cout << "\n";
     }
     return 0;
 }
