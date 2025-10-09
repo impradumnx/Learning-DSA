1a8233ae7f9cc28317a5768eb266a7d01a7baeb7
Pradum <pradumrathour944@gmail.com>
2025-10-09 22:55:17 +0530

    binary to decimal

---
+++ Week2/program13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int decTobi(int decNum){
+    int ans = 0, pow = 1;
+
+    while(decNum > 0){
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+
+    return ans;  // Binary form 
+}
+int main(){
+    int num;
+    cout << "Enter a decimal number : ";
+    cin >> num;
+    for(int i=1; i <= num; i++){
+        cout << decTobi(i) << endl;
+    }
+    return 0;
+}    
