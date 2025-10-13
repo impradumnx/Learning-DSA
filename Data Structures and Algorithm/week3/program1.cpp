af4069b9b252a2cbdaa1fb980b1c2ef7de1c8211
Pradum <pradumrathour944@gmail.com>
2025-10-13 22:06:56 +0530

    linear search in 1D array

---
+++ Data Structures and Algorithm/week3/program1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int linearsearch( int arr[], int size, int target){
+    for(int i=0; i<size; i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {4, 6, 0, 34 , 56, 18, 3};
+    int size = 7;
+    int target = 18;
+    cout << "Targeted element found on index : " << linearsearch( arr, size, target);
+    return 0;
+}
