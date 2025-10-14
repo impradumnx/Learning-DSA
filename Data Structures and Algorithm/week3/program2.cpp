b5dba40ea289cd4db453c3e7aeb113939a6e31f3
Pradum <pradumrathour944@gmail.com>
2025-10-14 18:47:40 +0530

    reverse of array

---
+++ Data Structures and Algorithm/week3/program2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void reversearray(int arr[], int size){
+    int start = 0, end = size-1;
+    while(start<end){
+        swap(arr[start], arr[end]);
+        start ++;
+        end --;
+    }
+}
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = 6;
+    reversearray(arr, size);
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
