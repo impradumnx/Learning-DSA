296c2d4ae35ffc3b6bebabeafd04707ca6281774
Pradum <pradumrathour944@gmail.com>
2025-10-16 23:32:35 +0530

    initialization of vector

---
+++ Data Structures and Algorithm/week3/program4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <int> value; // initial size = 0 
+    // to check the size 
+    cout << value.size() << endl;
+    return 0;
+}
