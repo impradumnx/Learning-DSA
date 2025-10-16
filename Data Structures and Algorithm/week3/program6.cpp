1f6cefc47c03312bab9677e06c355858105af985
Pradum <pradumrathour944@gmail.com>
2025-10-16 23:45:11 +0530

    to print all the elements of vector

---
+++ Data Structures and Algorithm/week3/program6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <int> value; // initial size = 0 
+    
+    value.push_back(1);
+    value.push_back(8);
+    value.push_back(3);
+    value.push_back(0);
+    value.push_back(4);
+    value.push_back(2);
+   // value.pop_back(); // to delete the last element
+   for(int val : value){
+    cout << val << " ";
+   }
+   cout << endl;
+
+return 0;
+}
