faa34c556a20c8460915e6b600ef0965a0702c0d
Pradum <pradumrathour944@gmail.com>
2025-10-16 23:37:38 +0530

    size & capacity of vector

---
+++ Data Structures and Algorithm/week3/program5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <int> value; // initial size = 0 
+    
+    value.push_back(1);
+    value.push_back(8);
+    value.push_back(3);
+
+    cout << "size = " << value.size() << endl; // 3
+    cout << "capacity = " << value.capacity() << endl; // 4
+    return 0;
+}
