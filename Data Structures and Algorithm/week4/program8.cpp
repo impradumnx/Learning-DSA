42cbfc6f519492930a8b6ba16964fadbad8dbf78
Pradum <pradumrathour944@gmail.com>
2026-01-29 21:07:45 +0530

    reverse string using stack

---
+++ Data Structures and Algorithm/week4/program8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+#include<stack>
+using namespace std;
+
+int main(){
+    char str[] ={" JAVA SCRIPT"};
+    stack<char> s;
+
+    for(int i=0; i<strlen(str); i++){
+        s.push(str[i]);
+    }
+    cout << "Reversed string : ";
+    while (!s.empty()){
+        cout << s.top();
+        s.pop();
+    }
+    
+    return 0;
+}
